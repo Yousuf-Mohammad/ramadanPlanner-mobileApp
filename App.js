c95ac2028b16db93d6d9ad0c202f1f47618bdd4d
Fardin96 <fardinshuvro96@gmail.com>
2024-02-14 21:53:11 +0600

    initial project setup with packages

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+      }}>
+      <Text style={{color: 'black'}}>RAMADAN-PLANNER</Text>
+    </View>
+  );
+};
+
+export default App;
